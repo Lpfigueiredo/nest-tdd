e9655c046408504e8d22a8574ddcf0517c23707e
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2021-01-05 23:33:24 -0300

    feat: ensure getCurrency throws if repository throws

--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,18 +1,42 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { CurrenciesService } from './currencies.service';
+import { CurrenciesRepository, CurrenciesService } from './currencies.service';
 
 describe('CurrenciesService', () => {
   let service: CurrenciesService;
+  let repository: CurrenciesRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CurrenciesService],
+      providers: [
+        CurrenciesService,
+        {
+          provide: CurrenciesRepository,
+          useFactory: () => ({
+            getCurrency: jest.fn(),
+          }),
+        },
+      ],
     }).compile();
 
     service = module.get<CurrenciesService>(CurrenciesService);
+    repository = module.get<CurrenciesRepository>(CurrenciesRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getCurrency()', () => {
+    it('should throw if repository throws', async () => {
+      (repository.getCurrency as jest.Mock).mockRejectedValue(new InternalServerErrorException());
+      await expect(service.getCurrency('INVALID')).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+
+    it('should not throw if repository not throws', async () => {
+      await expect(service.getCurrency('USD')).resolves.not.toThrow();
+    });
   });
 });
