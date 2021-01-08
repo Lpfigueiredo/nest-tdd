5a0f5b0edf574ead99586915b7bbcb56ad42719c
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2021-01-07 21:01:51 -0300

    feat: update currencies entity

--- src/currencies/currencies.entity.ts
+++ src/currencies/currencies.entity.ts
@@ -1,4 +1,20 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Unique(['currency'])
+@Entity()
 export class Currencies {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
   currency: string;
+
+  @Column()
   value: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
 }
